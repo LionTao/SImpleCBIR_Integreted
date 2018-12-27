8d3b6ba2d94c48c65a207de7171cc04346961383
Steve <stevehawk@qq.com>
2018-12-27 23:44:33 +0800

    Update upload_pic.py

--- main/upload_pic.py
+++ main/upload_pic.py
@@ -5,11 +5,12 @@ import json
 import os
 import tempfile
 import cv2
-import main.makecache as makecache
 from main.models import cache
 from main.img_preprocess import preprocess
 from modules.CNNCBIR import search_api
 import main.search_utils as search_utils
+import numpy as np
+from modules.ImageFeatureExtract.imgfeature import feature_color, feature_shape, feature_texture
 
 
 @csrf_exempt
@@ -41,17 +42,23 @@ def upload_pic(request):
                 usr_img_pre = preprocess(usr_img)
                 usr_img_pre_dir = temp_dir + 'usr_img_pre.jpg'
                 cv2.imwrite(usr_img_pre_dir, usr_img_pre)
+
+                search_utils.render_color_bar_figure(np.array(feature_color(img_in=usr_img_pre_dir)),
+                                                     temp_dir + 'usr_img_color.jpg')
+                search_utils.render_texture_bar_figure(np.array(feature_texture(img_in=usr_img_pre_dir)),
+                                                       temp_dir + 'usr_img_texture.jpg')
+                cv2.imwrite(temp_dir + 'usr_img_shape.jpg', np.array(feature_shape(img_in=usr_img_pre_dir)))
             else:
                 raise Exception("Preprocess Method Unknown.")
 
             # Similarity
             res_num = 3
+            imgNames = cache.path.objects.all()
+            feats = cache.cnn.objects.all()
             if similarityCalculationMethod == 'vector':
-                pass
+                result, score = search_utils.vec_search(usr_img_pre_dir, feats, imgNames, res_num)
             elif similarityCalculationMethod == 'cnn':
-                imgNames = cache.path.objects.all()
-                feats = cache.cnn.objects.all()
-                result = search_api.search_with_cnnData(usr_img_pre_dir, feats, imgNames, res_num)
+                result, score = search_api.search_with_cnnData(usr_img_pre_dir, feats, imgNames, res_num)
 
             for i in range(res_num):
                 cv2.imwrite(temp_dir + "res{}.jpg".format(i + 1), result[i])
@@ -60,6 +67,19 @@ def upload_pic(request):
             if positionMethod == 'od':
                 objd_image, catagory = search_utils.ObjDetect(usr_img_pre_dir)
                 cv2.imwrite(temp_dir + 'usr_objd.jpg', objd_image)
+            else:
+                raise Exception("Object Detection Method Unknown.")
+
+            # Feature Extraction + OD
+            if featureExtractionMethod == 'FOO_BAR':
+                pass
+            for i in range(res_num):
+                db_dict = list(cache.object.filter(path=result[i]).values())[0]
+                search_utils.render_color_bar_figure(db_dict['color'], temp_dir + 'color{}.jpg'.format(i + 1))
+                search_utils.render_texture_bar_figure(db_dict['texture'], temp_dir + 'texture{}.jpg'.format(i + 1))
+                cv2.imwrite(temp_dir + 'shape{}.jpg'.format(i + 1), db_dict['shape'])
+                od_temp, _ = search_utils.ObjDetect(result[i])
+                cv2.imwrite(temp_dir + 'position{}.jpg'.format(i + 1), od_temp)
 
             # ===============================
 
@@ -75,16 +95,16 @@ def upload_pic(request):
             return_message['position'] = temp_dir + 'usr_objd.jpg'  # path of the target marked image
             # 4
             features = dict()
-            features['color'] = 'FOO'  # path of the generated image
-            features['texture'] = 'FOO'  # path of the generated image
-            features['shape'] = 'FOO'  # path of the generated image
+            features['color'] = temp_dir + 'usr_img_color.jpg'  # path of the generated image
+            features['texture'] = temp_dir + 'usr_img_texture.jpg'  # path of the generated image
+            features['shape'] = temp_dir + 'usr_img_shape.jpg'  # path of the generated image
             return_message['features'] = features
 
             # 5
             results = dict()
             # 5.1
             one = dict()
-            one['similarity'] = 'FOO'
+            one['similarity'] = score[0]
             one['path'] = temp_dir + 'res1.jpg'
             one_features = dict()
             one_features['color'] = temp_dir + 'color1.jpg'
@@ -94,7 +114,7 @@ def upload_pic(request):
             one['features'] = one_features
             # 5.2
             two = dict()
-            two['similarity'] = 'FOO'
+            two['similarity'] = score[1]
             two['path'] = temp_dir + 'res2.jpg'
             two_features = dict()
             two_features['color'] = temp_dir + 'color2.jpg'
@@ -104,7 +124,7 @@ def upload_pic(request):
             two['features'] = two_features
             # 5.3
             three = dict()
-            three['similarity'] = 'FOO'
+            three['similarity'] = score[2]
             three['path'] = temp_dir + 'res1.jpg'
             three_features = dict()
             three_features['color'] = temp_dir + 'color3.jpg'
@@ -121,8 +141,6 @@ def upload_pic(request):
 
             # Write the result to file
             # ===============================
-            temp_dir = tempfile.gettempdir() + '/SimpleCBIR_ResultTemp'
-
             # Try to make the temp folder
             try:
                 os.mkdir(temp_dir)
@@ -131,14 +149,14 @@ def upload_pic(request):
 
             # Try to write the json file
             try:
-                busy = open(temp_dir + "/result.json.busy", 'w')
-                result_json = open(temp_dir + '/result.json', 'w')
+                busy = open(temp_dir + "result.json.busy", 'w')
+                result_json = open(temp_dir + 'result.json', 'w')
                 json.dump(return_message, result_json)
                 result_json.close()
                 busy.close()
                 import time
                 time.sleep(5)
-                os.remove(temp_dir + '/result.json.busy')
+                os.remove(temp_dir + 'result.json.busy')
 
             # Make sure everything is closed and deleted if anything bad occurs.
             except Exception as x:
@@ -148,7 +166,7 @@ def upload_pic(request):
                     print(str(e))
                     print("Busy Doesn't Exist.")
                 try:
-                    os.remove(temp_dir + '/result.json.busy')
+                    os.remove(temp_dir + 'result.json.busy')
                 except Exception as e:
                     print(str(e))
                     print("Busy Doesn't Exist.")
@@ -158,7 +176,7 @@ def upload_pic(request):
                     print(str(e))
                     print("Result Doesn't Exist.")
                 try:
-                    os.remove(temp_dir + '/result.json')
+                    os.remove(temp_dir + 'result.json')
                 except Exception as e:
                     print(str(e))
                     print("Result Doesn't Exist.")

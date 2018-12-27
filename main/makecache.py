942b7be6a9dc7712721bf90f67e09bb36802a374
LionTao <taojiachun980831@163.com>
2018-12-27 12:27:31 +0800

    beautify code

--- main/makecache.py
+++ main/makecache.py
@@ -1,23 +1,25 @@
+import tempfile
+import cv2
+from modules.CBIRDataset.getDataset import GetDataSet
+from modules.CNNCBIR.search_api import initCNNCache
+from modules.ImageFeatureVector.HistogramVector.hisvec import get_vector
+from modules.ImageFeatureExtract.imgfeature import feature_color, feature_shape, feature_texture
+from modules.CNNCBIR.search_api import search
+
+
 def getDataset():
-    import tempfile
-    from modules.CBIRDataset.getDataset import GetDataSet
     temp_dir = tempfile.gettempdir() + '/SimpleCBIR_ResultTemp/'
     GetDataSet(des=temp_dir)
     print("DataSet should be ready at ", temp_dir + '/dataset')
 
 
 def initCNNCBIR(dbpath):
-    import tempfile
-    from modules.CNNCBIR.search_api import initCNNCache
     temp_dir = tempfile.gettempdir() + '/SimpleCBIR_ResultTemp/'
     initCNNCache(dataset_path=temp_dir, dbpath=temp_dir)
     print("Database Cached at", dbpath + "index.sqlite")
 
 
 def make_image_cache(path):
-    from modules.ImageFeatureVector.HistogramVector.hisvec import get_vector
-    from modules.ImageFeatureExtract.imgfeature import feature_color, feature_shape, feature_texture
-    import cv2
     # vector
 
     img = cv2.imread(path)
@@ -47,23 +49,18 @@ def render_texture_bar_figure(texture_vector: list, path: str):
 
 
 if __name__ == '__main__':
-    from modules.CNNCBIR.search_api import search
+    temp_dir = tempfile.gettempdir() + '/SimpleCBIR_ResultTemp/'
+    getDataset()
+    initCNNCBIR(dbpath=temp_dir)
+    res = search(
+        imagepath="C:\\Users\\LionTao\\AppData\\Local\\Temp\\1\\SimpleCBIR_ResultTemp\\dataset\\art_1\\193000.jpg", k=3,
+        dbpath="C:\\Users\\LionTao\\AppData\\Local\\Temp\\1\\SimpleCBIR_ResultTemp")
+    print(res)
+    res = make_image_cache(
+        "C:\\Users\\LionTao\\AppData\\Local\\Temp\\1\\SimpleCBIR_ResultTemp\\dataset\\art_1\\193001.jpg")
+    print(res[3])
 
-    # import tempfile
-    #
-    # temp_dir = tempfile.gettempdir() + '/SimpleCBIR_ResultTemp/'
-    # getDataset()
-    # initCNNCBIR(dbpath=temp_dir)
-    # res = search(
-    #     imagepath="C:\\Users\\LionTao\\AppData\\Local\\Temp\\1\\SimpleCBIR_ResultTemp\\dataset\\art_1\\193000.jpg", k=3,
-    #     dbpath="C:\\Users\\LionTao\\AppData\\Local\\Temp\\1\\SimpleCBIR_ResultTemp")
-    # print(res)
-    # res = make_image_cache(
-    #     "C:\\Users\\LionTao\\AppData\\Local\\Temp\\1\\SimpleCBIR_ResultTemp\\dataset\\art_1\\193001.jpg")
-    # print(res[3])
-    # import cv2
-    #
-    # cv2.imshow("texture", res[3])
-    # cv2.waitKey(0)
+    cv2.imshow("texture", res[3])
+    cv2.waitKey(0)
 
-    # print(render_texture_bar_figure(res[3],"1.png"))
+    print(render_texture_bar_figure(res[3], "1.png"))
